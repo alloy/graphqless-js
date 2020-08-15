516c15460ddd9d868d5ec5a7a20c8f138df665d4
Eloy Durán <eloy.de.enige@gmail.com>
2020-08-16 01:13:02 +0200

    Add an executable test schema

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,37 @@
 import { helloWorld } from "../index";
 
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  graphql,
+} from "graphql";
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: () => ({
+      rootField: {
+        type: GraphQLString,
+        resolve: (source, args, context, info) => {
+          return "hello world";
+        },
+      },
+    }),
+  }),
+});
+
 describe(helloWorld, () => {
-  it("works", () => {
-    expect(helloWorld()).toBe("hello world");
+  it("works", async () => {
+    const { data } = await graphql({
+      schema,
+      source: `
+        {
+          rootField
+        }
+      `,
+    });
+
+    expect(data!.rootField).toBe("hello world");
   });
 });
