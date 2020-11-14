d593466c3f4c2c9b08da635771d5efab96f1ca07
Eloy Durán <eloy.de.enige@gmail.com>
2020-11-14 14:27:13 +0100

    Extract resolver function builders

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,140 @@ const objectTypeFieldResolverBuilder = template.expression(`
   }
 `);
 
+function compileScalarTypeResolver({
+  typeInfo,
+  fieldNode,
+  argumentProperties,
+  source,
+  emitAST
+}: {
+  typeInfo: g.TypeInfo,
+  fieldNode: g.FieldNode,
+  argumentProperties: t.ObjectProperty[],
+  source: t.Identifier,
+  emitAST: boolean
+}): [t.Expression, boolean] {
+  const parentType = typeInfo.getParentType();
+  invariant(
+    g.isObjectType(parentType),
+    "Expected parentType to be an object type"
+  );
+  let expression: t.Expression;
+  let hasAsyncResolver: boolean = false;
+  const fieldConfig = parentType.toConfig().fields[
+    fieldNode.name.value
+  ];
+  if (fieldConfig.resolve) {
+    // Generate AST for JSON representation of GraphQL AST
+    // TODO: Merge multiple field selections
+    let fieldNodes: t.ExpressionStatement | null = null;
+    if (emitAST) {
+      fieldNodes = parseJS(JSON.stringify([fieldNode])).program
+        .body[0] as t.ExpressionStatement;
+    }
+
+    expression = invokeFieldResolverBuilder({
+      source,
+      args: t.objectExpression(argumentProperties),
+      typeName: t.stringLiteral(parentType.name),
+      fieldName: t.identifier(fieldNode.name.value),
+      resolveInfo: t.objectExpression(
+        fieldNodes === null
+          ? []
+          : [
+              t.objectProperty(
+                t.identifier("fieldNodes"),
+                fieldNodes.expression
+              ),
+            ]
+      ),
+    });
+
+    // TODO Optionally handle Promise return results when not an AsyncFunction
+    const resolver = typeInfo.getFieldDef().resolve;
+    if (resolver && resolver.constructor.name === "AsyncFunction") {
+      hasAsyncResolver = true;
+      expression = t.awaitExpression(expression);
+    }
+  } else {
+    expression = invokeDefaultFieldResolverBuilder({
+      source,
+      fieldName: t.stringLiteral(fieldNode.name.value),
+    });
+  }
+  return [expression, hasAsyncResolver]
+}
+
+function compileObjectTypeResolver({
+  typeInfo,
+  fieldNode,
+  argumentProperties,
+  source,
+  parentSource,
+  selectionSet,
+  markAsync
+}: {
+  typeInfo: g.TypeInfo,
+  fieldNode: g.FieldNode,
+  argumentProperties: t.ObjectProperty[],
+  source: t.Identifier,
+  parentSource: t.Identifier,
+  selectionSet: t.ObjectExpression,
+  markAsync: boolean
+}): t.Expression {
+  const parentType = typeInfo.getParentType();
+  invariant(
+    g.isObjectType(parentType),
+    "Expected parentType to be an object type"
+  );
+  // invariant(g.isObjectType(type), "Expected a object type");
+  let invokeObjectFieldResolver: t.Expression;
+  invariant(
+    g.isObjectType(parentType),
+    "Expected parentType to be an object type"
+  );
+  const fieldConfig = parentType.toConfig().fields[
+    fieldNode.name.value
+  ];
+  if (fieldConfig.resolve) {
+    invokeObjectFieldResolver = invokeFieldResolverBuilder({
+      source: parentSource,
+      args: t.objectExpression(argumentProperties),
+      typeName: t.stringLiteral(parentType.name),
+      fieldName: t.identifier(fieldNode.name.value),
+      resolveInfo: undefined,
+    });
+  } else {
+    invokeObjectFieldResolver = invokeDefaultFieldResolverBuilder({
+      source: parentSource,
+      fieldName: t.stringLiteral(fieldNode.name.value),
+    });
+  }
+
+  let expression: t.Expression
+  // Create resolver function
+  expression = objectTypeFieldResolverBuilder({
+    source,
+    invokeObjectFieldResolver,
+    selectionSet,
+  });
+  if (markAsync) {
+    // Mark it async, if necessary
+    expression = {
+      ...(expression as t.FunctionExpression),
+      async: true,
+    };
+  }
+  // Invoke
+  expression = t.callExpression(expression, []);
+  if (markAsync) {
+    // Await it, if it was async
+    expression = t.awaitExpression(expression);
+  }
+
+  return expression
+}
+
 function transformOperations(
   source: g.DocumentNode,
   schema: g.GraphQLSchema,
@@ -164,103 +298,22 @@ function transformOperations(
           }
 
           if (g.isScalarType(type)) {
-            invariant(
-              g.isObjectType(parentType),
-              "Expected parentType to be an object type"
-            );
-            const fieldConfig = parentType.toConfig().fields[
-              fieldNode.name.value
-            ];
-            if (fieldConfig.resolve) {
-              // Generate AST for JSON representation of GraphQL AST
-              // TODO: Merge multiple field selections
-              let fieldNodes: t.ExpressionStatement | null = null;
-              if (emitAST) {
-                fieldNodes = parseJS(JSON.stringify([fieldNode])).program
-                  .body[0] as t.ExpressionStatement;
-              }
-
-              expression = invokeFieldResolverBuilder({
-                source,
-                args: t.objectExpression(argumentProperties),
-                typeName: t.stringLiteral(parentType.name),
-                fieldName: t.identifier(fieldNode.name.value),
-                resolveInfo: t.objectExpression(
-                  fieldNodes === null
-                    ? []
-                    : [
-                        t.objectProperty(
-                          t.identifier("fieldNodes"),
-                          fieldNodes.expression
-                        ),
-                      ]
-                ),
-              });
-
-              // TODO Optionally handle Promise return results when not an AsyncFunction
-              const resolver = typeInfo.getFieldDef().resolve;
-              if (resolver && resolver.constructor.name === "AsyncFunction") {
-                markAncestorFunctionsAsync();
-                expression = t.awaitExpression(expression);
-              }
-            } else {
-              expression = invokeDefaultFieldResolverBuilder({
-                source,
-                fieldName: t.stringLiteral(fieldNode.name.value),
-              });
+            const [resolverExpression, hasAsyncResolver] = compileScalarTypeResolver({ typeInfo, fieldNode, argumentProperties, source, emitAST })
+            expression = resolverExpression
+            if (hasAsyncResolver) {
+              markAncestorFunctionsAsync()
             }
           } else {
-            invariant(g.isObjectType(type), "Expected a object type");
+            sourceStack.pop();
+            const parentSource = sourceStack[sourceStack.length - 1]
             invariant(
               currentSelectionSet,
               "Expected there to be a current selection for object type"
             );
-            sourceStack.pop();
-            let invokeObjectFieldResolver: t.Expression;
-            invariant(
-              g.isObjectType(parentType),
-              "Expected parentType to be an object type"
-            );
-            const fieldConfig = parentType.toConfig().fields[
-              fieldNode.name.value
-            ];
-            if (fieldConfig.resolve) {
-              invokeObjectFieldResolver = invokeFieldResolverBuilder({
-                source: sourceStack[sourceStack.length - 1],
-                args: t.objectExpression(argumentProperties),
-                typeName: t.stringLiteral(parentType.name),
-                fieldName: t.identifier(fieldNode.name.value),
-                resolveInfo: undefined,
-              });
-            } else {
-              invokeObjectFieldResolver = invokeDefaultFieldResolverBuilder({
-                source: sourceStack[sourceStack.length - 1],
-                fieldName: t.stringLiteral(fieldNode.name.value),
-              });
-            }
-
-            // Create resolver function
-            expression = objectTypeFieldResolverBuilder({
-              source,
-              invokeObjectFieldResolver,
-              selectionSet: currentSelectionSet,
-            });
-            const markAsync = markFunctionAsyncStack.pop();
-            if (markAsync) {
-              // Mark it async, if necessary
-              expression = {
-                ...(expression as t.FunctionExpression),
-                async: true,
-              };
-            }
-            // Invoke
-            expression = t.callExpression(expression, []);
-            if (markAsync) {
-              // Await it, if it was async
-              expression = t.awaitExpression(expression);
-            }
-
+            const selectionSet = currentSelectionSet
             currentSelectionSet = null;
+            const markAsync = !!markFunctionAsyncStack.pop();
+            expression = compileObjectTypeResolver({ typeInfo, fieldNode, argumentProperties, source, parentSource, selectionSet, markAsync })
           }
           selectionSetStack[selectionSetStack.length - 1].push(
             t.objectProperty(t.identifier(fieldNode.name.value), expression)
